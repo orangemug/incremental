f5e69782286b5564eff8d4713bb9a75cc194ecf8
orangemug <info@orangemug.co.uk>
2014-03-01 18:07:33 +0000

    Tidy tests.

--- test/index.js
+++ test/index.js
@@ -3,10 +3,16 @@ var test = require('tape');
 
 
 function fakeEvent(keyCode, obj) {
+  var target = document.createElement("input");
+  document.body.appendChild(target);
+
   obj = obj || {};
   obj.target = target;
   obj.keyCode = keyCode;
   obj.preventDefault = function() {};
+  obj.destroy = function() {
+    document.body.removeChild(target);
+  }
   return obj;
 }
 
@@ -42,129 +48,148 @@ var testFuns = {
   }
 };
 
-var target = document.createElement("input");
-document.body.appendChild(target);
-
 test('increase when shift+UP pressed', function(t) {
-  target.value = "30";
   var e = fakeEvent(KEYS.up, {shiftKey: true});
+  e.target.value = "30";
   incr._hdl(undefined, e);
-  t.equal(target.value, "40");
+  t.equal(e.target.value, "40");
+
+  e.destroy();
   t.end();
 });
 
 test('decrease when shift+DOWN pressed', function(t) {
-  target.value = "30";
   var e = fakeEvent(KEYS.down, {shiftKey: true});
+  e.target.value = "30";
   incr._hdl(undefined, e);
-  t.equal(target.value, "20");
+  t.equal(e.target.value, "20");
+
+  e.destroy();
   t.end();
 });
 
 test('increase when alt+UP pressed', function(t) {
-  target.value = "30.3";
   var e = fakeEvent(KEYS.up, {altKey: true});
+  e.target.value = "30.3";
   incr._hdl(undefined, e);
-  t.equal(target.value, "30.4");
+  t.equal(e.target.value, "30.4");
+
+  e.destroy();
   t.end();
 });
 
 test('decrease when alt+DOWN pressed', function(t) {
-  target.value = "30.3";
   var e = fakeEvent(KEYS.down, {altKey: true});
+  e.target.value = "30.3";
   incr._hdl(undefined, e);
-  t.equal(target.value, "30.2");
+  t.equal(e.target.value, "30.2");
+
+  e.destroy();
   t.end();
 });
 
 test('decrease by FLOAT when mod+UP pressed on INT', function(t) {
-  target.value = "30";
   var e = fakeEvent(KEYS.up);
+  e.target.value = "30";
   incr._hdl({modifier: testFuns.ret0pt1}, e);
-  t.equal(target.value, "30.01");
+  t.equal(e.target.value, "30.01");
+
+  e.destroy();
   t.end();
 });
 
 test('increase by FLOAT when mod+DOWN pressed on INT', function(t) {
-  target.value = "30";
   var e = fakeEvent(KEYS.down);
+  e.target.value = "30";
   incr._hdl({modifier: testFuns.ret0pt1}, e);
-  t.equal(target.value, "29.99");
+  t.equal(e.target.value, "29.99");
+
+  e.destroy();
   t.end();
 });
 
 test('decrease by INT when mod+UP pressed on INT', function(t) {
-  target.value = "30";
   var e = fakeEvent(KEYS.up);
+  e.target.value = "30";
   incr._hdl({modifier: testFuns.ret1}, e);
-  t.equal(target.value, "31");
+  t.equal(e.target.value, "31");
+
+  e.destroy();
   t.end();
 });
 
 test('increase by INT when mod+DOWN pressed on INT', function(t) {
-  target.value = "30";
   var e = fakeEvent(KEYS.down);
+  e.target.value = "30";
   incr._hdl({modifier: testFuns.ret1}, e);
-  t.equal(target.value, "29");
+  t.equal(e.target.value, "29");
+
+  e.destroy();
   t.end();
 });
 
 test('decrease by FLOAT when mod+UP pressed on FLOAT', function(t) {
-  target.value = "30.1";
   var e = fakeEvent(KEYS.up);
+  e.target.value = "30.1";
   incr._hdl({modifier: testFuns.ret0pt1}, e);
-  t.equal(target.value, "30.11");
+  t.equal(e.target.value, "30.11");
+
+  e.destroy();
   t.end();
 });
 
 test('increase by FLOAT when mod+DOWN pressed on FLOAT', function(t) {
-  target.value = "30.1";
   var e = fakeEvent(KEYS.down);
+  e.target.value = "30.1";
   incr._hdl({modifier: testFuns.ret0pt1}, e);
-  t.equal(target.value, "30.09");
+  t.equal(e.target.value, "30.09");
+
+  e.destroy();
   t.end();
 });
 
 test('increase by FLOAT when mod+DOWN pressed on FLOAT', function(t) {
-  target.value = "30.1";
   var e = fakeEvent(KEYS.down);
+  e.target.value = "30.1";
   incr._hdl({modifier: testFuns.ret0pt1}, e);
-  t.equal(target.value, "30.09");
+  t.equal(e.target.value, "30.09");
+
+  e.destroy();
   t.end();
 });
 
 test('support for a partial number string', function(t) {
-  target.value = "top: 1.2px";
-  selectRange(target, 6);
   var e = fakeEvent(KEYS.down);
+  e.target.value = "top: 1.2px";
+  selectRange(e.target, 6);
   incr._hdl({modifier: testFuns.ret0pt1, partials: true}, e);
-  t.equal(target.value, "top: 1.19px");
+  t.equal(e.target.value, "top: 1.19px");
+
+  e.destroy();
   t.end();
 });
 
 test('support for multiple partial numbers string', function(t) {
-  target.value = "rgba(225, 200, 100, 0.5)";
-
-  selectRange(target, 5);
   var e = fakeEvent(KEYS.down);
+  e.target.value = "rgba(225, 200, 100, 0.5)";
+  selectRange(e.target, 5);
+
   incr._hdl({modifier: testFuns.ret1, partials: true}, e);
-  t.equal(target.value, "rgba(224, 200, 100, 0.5)");
+  t.equal(e.target.value, "rgba(224, 200, 100, 0.5)");
 
-  selectRange(target, 11);
-  var e = fakeEvent(KEYS.down);
+  selectRange(e.target, 11);
   incr._hdl({modifier: testFuns.ret1, partials: true}, e);
-  t.equal(target.value, "rgba(224, 199, 100, 0.5)");
+  t.equal(e.target.value, "rgba(224, 199, 100, 0.5)");
 
-  selectRange(target, 17);
-  var e = fakeEvent(KEYS.down);
+  selectRange(e.target, 17);
   incr._hdl({modifier: testFuns.ret1, partials: true}, e);
-  t.equal(target.value, "rgba(224, 199, 99, 0.5)");
+  t.equal(e.target.value, "rgba(224, 199, 99, 0.5)");
 
-  selectRange(target, 20);
-  var e = fakeEvent(KEYS.down);
+  selectRange(e.target, 20);
   incr._hdl({modifier: testFuns.ret0pt1, partials: true}, e);
-  t.equal(target.value, "rgba(224, 199, 99, 0.49)");
+  t.equal(e.target.value, "rgba(224, 199, 99, 0.49)");
 
+  e.destroy();
   t.end();
 });
 
